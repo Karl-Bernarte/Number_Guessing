8a8eb482d489ec50c9c110b842d3bdcf75cecede
Karl Bernarte <karlshanegt@yahoo.com>
2025-02-06 12:45:40 +0000

    fix: smooth out bugs

--- number_guess.sh
+++ number_guess.sh
@@ -4,7 +4,7 @@ SECRET_NUMBER=$(( $RANDOM % 1000 + 1 ))
 echo "Enter your username: "
 read USERNAME
 
-RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = $USERNAME")
+RETURNING_USER=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
 
 if [[ -z $RETURNING_USER ]]
 then
@@ -12,35 +12,36 @@ then
   INSERTED_USER=$($PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
 
 else
-GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
-BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN USING(user_id) WHERE username = $USERNAME")
+GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games INNER JOIN users USING(user_id) WHERE username = $USERNAME")
+BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games INNER JOIN users USING(user_id) WHERE username = $USERNAME")
 
 echo GAMES_PLAYED
 echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
 fi
 
+echo "$SECRET_NUMBER"
 echo "Guess the secret number between 1 and 1000:"
 read  GUESS
 TRIES=1
 while [ ! $GUESS -eq $SECRET_NUMBER ] 
 do
 
-  TRIES=$TRIES+1
+  TRIES=$(expr $TRIES + 1)
   
-  if [[ ! $GUESS =~ ^[0-9]+$]]
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
     read GUESS
   else
 
-    if [[ $GUESS -gt $SECRET_NUMBER]]
+    if [[ $GUESS -lt $SECRET_NUMBER ]]
     then
 
       echo "It's higher than that, guess again:"
       read GUESS
 
-    elif [[ $GUESS -lt $SECRET_NUMBER ]]
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
     then
     
       echo "It's lower than that, guess again:"
