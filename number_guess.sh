6611a6fed7106cd3587cdf945a9a987e87258225
Karl Bernarte <karlshanegt@yahoo.com>
2025-02-06 14:25:06 +0000

    fix: update guessing logic

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This is a test change
-
+# Testing again
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUMBER=$(( $RANDOM % 1000 + 1 ))
 echo "Enter your username: "
